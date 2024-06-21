3aba46a8439efab170f7174b79924f49ad40f847
m-mburu <mmburu20@gmail.com>
2024-06-21 11:04:09 +0300

    change output dir

--- R/job.R
+++ R/job.R
@@ -1,4 +1,9 @@
 
-library(rmarkdown)
+#library(rmarkdown)
 
-render("R/README2.Rmd", output_format = "github_document", output_file = "R/README2.md")
+#render("R/README2.Rmd", output_format = "github_document", output_file = "R/README2.md")
+
+sample <- sample(1:150, sample(1:10, 1))
+iris_subset <- iris[sample, ]
+
+write.csv(iris_subset, "data/iris_subset.csv")
