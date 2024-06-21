e1a055b72fca2e9a0a2cd40ff992d0a02292cdc2
m-mburu <mmburu20@gmail.com>
2024-06-21 11:29:55 +0300

    change output dir

--- R/job.R
+++ R/job.R
@@ -3,7 +3,7 @@
 
 #render("R/README2.Rmd", output_format = "github_document", output_file = "R/README2.md")
 
-sample <- sample(1:150, sample(1:10, 1))
+sample <- sample(1:150, sample(1:50, 1))
 iris_subset <- iris[sample, ]
 
 write.csv(iris_subset, "data/iris_subset.csv")
